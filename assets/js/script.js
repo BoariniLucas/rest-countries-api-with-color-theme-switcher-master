812675020775e8f5a706114798da101f02008c7e
Lucas Boarini <lcsboarinireis92@gmail.com>
2023-06-19 09:56:46 -0300

    :construction: Estruturando projeto e inicio de desenvolvimeto do botão switch theme

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+const switchTheme = document.querySelector('#switch-theme');
+
+switchTheme.addEventListener('change', () => {
+    console.log("mudou");
+});
+
+/* continuar desenvolvimento do botão switch */
