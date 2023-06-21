c4987c2eb644313a9c1fa950b9584d17110d8743
Lucas Boarini <lcsboarinireis92@gmail.com>
2023-06-21 10:20:07 -0300

    :sparkles: feat: Função de troca de thema implementada, atualizar apenas as cores

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,29 @@
 const switchTheme = document.querySelector('#switch-theme');
+const btnImgTheme = document.querySelector('#btn-img-theme');
+
+let theme = 1;
 
 switchTheme.addEventListener('change', () => {
-    console.log("mudou");
-});
+    
+    if(theme == 1) {
 
-/* continuar desenvolvimento do botão switch */
+        btnImgTheme.src = "./assets/images/moon-fill.svg";
+
+        document.body.style.setProperty('--headerTextColor', '#ffffff');
+        document.body.style.setProperty('--headerDivisorColor', '#1e2b35');        
+        document.body.style.setProperty('--headerBgColor', '#2b3743');
+
+        theme = 2;
+
+    } else {
+
+        btnImgTheme.src = "./assets/images/moon.svg";
+
+        document.body.style.setProperty('--headerTextColor', '#000000');
+        document.body.style.setProperty('--headerDivisorColor', '#efefef');
+        document.body.style.setProperty('--headerBgColor', 'none');
+
+        theme = 1;
+    }
+
+});
